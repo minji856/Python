014e0c7e436b1bcab219ed6f7cce8eade77c6ea1
minji856 <sistar96@gmail.com>
2024-01-21 20:47:16 +0900

    :sparkles: 정렬

---
+++ List/list06.py
@@ -0,0 +1,10 @@
+# 오름차순 정렬
+list_a = [52, 273, 101, 32, 275, 1, 7]
+print(list_a)
+list_a.sort()
+print("오름차순 정렬: ", list_a)
+print()
+
+# 내림차순 정렬
+list_a.sort(reverse=True) # 파이썬에선 대문자로 True
+print("내림차순 정렬: ", list_a)
