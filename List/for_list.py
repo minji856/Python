5ed30fc5c24ba9f9ffde2ef1ad4ec98945831c06
minji856 <sistar96@gmail.com>
2024-01-22 11:53:53 +0900

    :sparkles: for문

---
+++ List/for_list.py
@@ -0,0 +1,11 @@
+# 리스트를 선언합니다
+array = [273, 32, 103, 57, 52]
+
+# 리스트에 반복문을 적용합니다
+# for item으로 해도 출력됨 : 변수 이름의 역할
+for element in array:
+    print(element)
+
+# 문자열과 함께 사용할 수 있습니다
+for charcter in "안녕하세요":
+    print("-", charcter)
