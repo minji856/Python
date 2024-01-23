66b74c0fd5bf7161c780e8932f39878827f401b4
minji856 <sistar96@gmail.com>
2024-01-23 13:12:09 +0900

    :sparkles: 도전문제

--- List/testlist.py
+++ List/testlist.py
@@ -16,6 +16,7 @@ for number in numbers:
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 output = [[], [], []]
 
+# 이해하기 어려운 부분 !
 for number in numbers:
     output[(number + 2) % 3].append(number)
 print(output)
