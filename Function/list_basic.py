2d5a70468ea6c3f1c88320d9e1fc24ef5c739613
minji856 <sistar96@gmail.com>
2024-02-07 17:47:46 +0900

    :sparkles: 리스트 기본 함수 min,max,sum

---
+++ Function/list_basic.py
@@ -0,0 +1,13 @@
+# 리스트에 적용할 수 있는 기본 함수
+
+numbers = [103, 52, 273, 32, 77]
+
+print("numbers :",numbers)
+print("최소값 min(numbers) :", min(numbers))
+print("최대값 max(numbers) :", max(numbers))
+print("모두 더한 값 sum(numbers) :",sum(numbers))
+print()
+
+# 매개변수에 리스트를 사용 하지 않아도 숫자 여러개 나열해서 찾을 수도 있다,
+print("# 매개변수에 리스트를 사용 하지 않아도 숫자 여러개 나열해서 찾을 수도 있다,")
+print("min(243, 233, 4) :", min(243, 233, 4))
