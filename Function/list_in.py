ec5f34748cc71b41a245d5435cbf2edb6e7a12c4
minji856 <sistar96@gmail.com>
2024-03-22 18:29:04 +0900

    :sparkles: 리스트내포 리스트안에 for문

--- Function/list_in.py
+++ Function/list_in.py
@@ -3,3 +3,8 @@
 # [ 표현식 for 반복자 in 반복할 수 있는 것 (if 조건문)]
 array = [i*i for i in range(0, 20, 2)]
 print(array)
+
+array = ["사과", "자두", "초콜릿", "바나나", "체리"]
+output = [fruit for fruit in array if  fruit != "초콜릿"]
+# array 를 출력하면 초콜릿 표함 된 채로 출력됨
+print(output)
